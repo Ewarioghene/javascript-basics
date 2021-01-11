6541ac0c3005be814d736b61c36b90c1c258ec28
Daveena Uwuseba <daveuwus@gmail.com>
2021-01-11 04:13:38 -0800

    committed new files

---
+++ index.js
@@ -0,0 +1,98 @@
+//let firstName='Daveena';
+//let lastName ='Uwuseba';
+//console.log(firstName);
+//console.log(lastName);
+
+//const interestRate = 0.3
+//console.log (interestRate);
+
+//primitivetypes
+//let name = 'Daveena'
+//console.log(name);
+//let age = 18;
+//console.log(age);
+//let isapproved = true;
+//console.log(isapproved);
+//let firstName = undefined;
+//console.log(firstName);
+//let selectedColor = null;
+//console.log(selectedColor);
+
+//reference types
+//let person= {
+//name: 'Daveena',
+//age: 18 
+//}
+
+//Dot notation
+//person.name = 'John'
+//console.log(person.name);
+
+//bracket notation
+//person ['age'] = '22';
+//console.log(person.age);
+
+//array
+//let fruits = ['apple', 'pineapple','orange'];
+//fruits[3]='coconut';
+//console.log(fruits.length);
+
+//functions
+//function greet(name, lastName ) {
+    //console.log('Hello '+  name + ' ' + lastName);
+//}
+//greet('Mary', 'Lazarus');
+
+//calculating a value
+/*function square(number){
+   return  number * number;
+} 
+console.log (square(9));
+
+
+class Rectangle {
+    constructor () {
+        console.log("The Rectangle is being created!");
+    }
+
+}
+*/
+
+/*let a = 100;
+let b = 100;
+if ( a > b) {
+    console.log ("a is greater than b");
+} else {
+     console.log ("a is less than or equal to b") ;
+}
+*/
+
+
+/*var firstNumber= 12;
+var secondNumber = 15;
+console.log (firstNumber % 3);
+console.log (secondNumber % 2);
+*/
+/*
+var firstNumber=(12 % 7);
+var secondNumber=(15 % 2);
+console.log (firstNumber);
+    if (firstNumber = 0){
+    console.log("even");
+} else {
+    console.log ("odd");
+}
+*/
+
+
+
+var name = 1
+if ( name = "daveena") {
+    console.log ("Hello Daveena!");
+} else {
+    console.log("no name input");
+}
+
+
+
+
